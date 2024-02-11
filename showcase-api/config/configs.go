369ede9169555d96d371df7297fe309fe535af6e
Akash Jain <ajcodes.tech@gmail.com>
2024-02-11 18:32:05 +0530

    refactor

--- showcase-api/config/configs.go
+++ showcase-api/config/configs.go
@@ -19,27 +19,22 @@ type ServerConfig struct {
 
 func NewConfig(l *log.Logger) *Config {
 	config := &Config{l, &ServerConfig{}}
-	configPath := config.getPath()
-	yamlFile, err := os.ReadFile(configPath)
-
+	yamlFile, err := os.ReadFile(config.getPath())
 	if err != nil {
 		panic("file reading failed")
 	}
 	if err = yaml.Unmarshal(yamlFile, config); err != nil {
 		panic("config un-marshal failed")
 	}
-	l.Printf("%v %v", string(yamlFile), config.SConfig)
 	return config
 }
 
 func (c *Config) Port() string {
-	c.l.Printf("port := %s\n", c.SConfig.Port)
 	return c.SConfig.Port
 }
 
 func (c *Config) getPath() string {
 	args := os.Args
 	path := fmt.Sprintf("envs/%s.yaml", args[1])
-	c.l.Println(path)
 	return path
 }
